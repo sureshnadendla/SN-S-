3b9b21c300695d30b903e58d7b122f81a07d5fde
sureshnadendla <nadendla.sureshbabu@yahoo.com>
2015-06-21 10:59:20 +0530

    basic site

---
+++ js/custom_js3a05.js
@@ -0,0 +1,16 @@
+
+
+
+
+var $j = jQuery.noConflict();
+
+$j(document).ready(function() {
+
+
+$j( ".menu-item" ).mousedown(function() {
+	$j(this).trigger('focusout');
+});
+
+	"use strict";
+
+	});
